b487885f603a7c0c6c5120fc9eb30c1dc2611409
adam.bark <adam.bark@warburtons.co.uk>
2024-10-10 13:37:00 +0000

    init

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,9 @@
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:140476852.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1474245325.
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
